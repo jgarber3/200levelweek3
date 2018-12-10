966277264c4d9da0a6c96f4eed85c839b0b970ce
jgarber3 <johnwgarber3@gmail.com>
2018-12-10 17:06:59 -0500

    Add files via upload

---
+++ 200week3-chapter12.5.1.R
@@ -0,0 +1,12 @@
+#12.5.1
+
+#1 Compare and contrast fill and complete
+#answer 
+# Compare and contrast questions suck and I hope this never happens again
+#second answer
+# complete takes missing values and fills them will NA. Turning implicit missing into explicit missing. Fill takes explicit missing values and turns
+# them into the last listed actual value.
+
+
+#2 What does the direction parameter of fill do?
+#answer it deterins if the filling value comes from the last valid value BEFORE (parameter = DOWN) or if it comes from the last valid value AFTER (parameter = UP)
